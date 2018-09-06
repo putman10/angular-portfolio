94b47a8eef200990f826d42adcdd1d74964504e8
Ryan Putman <putman10@me.com>
2018-09-06 15:26:59 -0700

    add admin styling to match portfolio component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../project.service';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [ProjectService]
 })
 export class AdminComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private projectService: ProjectService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
+  }
+
+  editProject(selectedProjectKey){
+    this.router.navigate(['projects', selectedProjectKey, 'edit'])
+  }
+
+  deleteProject(selectedProjectKey) {
+    let projectInFirebase = this.projectService.getProjectById(selectedProjectKey);
+    projectInFirebase.remove();
+    this.router.navigate(['admin']);
+  }
+
+  addNewArticle(){
+    this.router.navigate(['projects/new']);
   }
 
 }
