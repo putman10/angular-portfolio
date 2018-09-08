48544591e4b29621dcfdb8c617d22c37540330ba
Ryan Putman <putman10@me.com>
2018-09-07 19:09:32 -0700

    add animation on scroll to component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,24 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import { ProjectService } from '../project.service';
 import { Project } from '../models/project.model';
 import { Router } from '@angular/router';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css'],
-  providers: [ProjectService]
+  providers: [ProjectService],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+        transform: "translateX(0)"
+      })),
+      state('hide',   style({
+        opacity: 0,
+        transform: "translateX(-100%)"
+      })),
+      transition('show => hide', animate('700ms ease-out')),
+      transition('hide => show', animate('700ms ease-in'))
+    ])
+  ]
 })
 export class PortfolioComponent implements OnInit {
 
   projects: FirebaseListObservable<any[]>;
   selectedCategory = "View All";
-  bounce: any;
+  state = 'hide';
 
-    constructor(private router: Router, private projectService: ProjectService, private route: ActivatedRoute, private location: Location) { }
+    constructor(private router: Router, private projectService: ProjectService, private route: ActivatedRoute, private location: Location, public el: ElementRef) { }
 
     ngOnInit() {
       this.projects = this.projectService.getProjects();
@@ -30,6 +45,20 @@ export class PortfolioComponent implements OnInit {
 
     viewProject(clickedProjectKey){
     this.router.navigate(['projects', clickedProjectKey])
-  }
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    checkScroll() {
+      const componentPosition = this.el.nativeElement.offsetTop
+      const scrollPosition = window.pageYOffset
+
+      if (scrollPosition >= componentPosition) {
+        this.state = 'show'
+      } else {
+        this.state = 'hide'
+      }
+
+    }
+
 
 }
