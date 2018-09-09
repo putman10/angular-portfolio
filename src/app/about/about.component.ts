1bf542d2110ddfd6c5cafa0ae9be11cbfb78797a
Ryan Putman <putman10@me.com>
2018-09-09 10:54:43 -0700

    finish resume sectiume section. Html css and animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+      })),
+      state('hide',   style({
+        opacity: 0,
+      })),
+      transition('show => hide', animate('500ms ease-out')),
+      transition('hide => show', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
+  state = 'hide';
 
-  constructor() { }
+  constructor(public el: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop
+    const scrollPosition = window.pageYOffset
+
+    if (scrollPosition >= componentPosition) {
+      this.state = 'show'
+    } else {
+      this.state = 'hide'
+    }
+
   }
 
 }
--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: ['./resume.component.css'],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+      })),
+      state('hide',   style({
+        opacity: 0,
+      })),
+      transition('show => hide', animate('500ms ease-out')),
+      transition('hide => show', animate('500ms ease-in'))
+    ]),
+    trigger('flyInRight', [
+      state('show', style({
+        opacity: 1,
+        transform: "translateX(0)"
+      })),
+      state('hide',   style({
+        opacity: 0,
+        transform: "translateX(100%)"
+      })),
+      transition('show => hide', animate('500ms ease-out')),
+      transition('hide => show', animate('500ms ease-in'))
+    ]),
+    trigger('flyInLeft', [
+      state('show', style({
+        opacity: 1,
+        transform: "translateX(0)"
+
+      })),
+      state('hide',   style({
+        opacity: 0,
+        transform: "translateX(-100%)"
+      })),
+      transition('show => hide', animate('500ms ease-out')),
+      transition('hide => show', animate('500ms ease-in'))
+    ])
+  ]
 })
 export class ResumeComponent implements OnInit {
+  state = 'hide';
 
-  constructor() { }
+  constructor(public el: ElementRef) { }
 
   ngOnInit() {
   }
 
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.el.nativeElement.offsetTop
+    const scrollPosition = window.pageYOffset
+
+    if (scrollPosition >= componentPosition) {
+      this.state = 'show'
+    } else {
+      this.state = 'hide'
+    }
+
+  }
 }
